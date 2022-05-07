5a2436cf096076481bcf22cb02658f11a11fdca8
mipe <103583498+mipe1@users.noreply.github.com>
2022-05-07 20:33:51 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,23 @@
 local f = game:GetService("Players").LocalPlayer
 
+local t = false
+
 script.Parent.MouseButton1Click:Connect(function()
-	for i,v in pairs(f:GetDescendants()) do
-		if f.Character:WaitForChild("GRABBING_CONSTRAINT") then
-			f.Character:WaitForChild("GRABBING_CONSTRAINT"):Destroy()
-			wait(.1)
-			f.Character:WaitForChild("Humanoid").Sit = true
-			wait(.2)
-			f.Character:WaitForChild("Humanoid").Sit = false
+	if t == false then
+		t = true
+		for i,v in pairs(f:GetDescendants()) do
+			if f.Character:WaitForChild("GRABBING_CONSTRAINT") then
+				f.Character:WaitForChild("GRABBING_CONSTRAINT"):Destroy()
+				wait(.1)
+				f.Character:WaitForChild("Humanoid").Sit = true
+				wait(.2)
+				f.Character:WaitForChild("Humanoid").Sit = false
+			end
+			if t == false then
+				break
+			end
 		end
+	elseif t == true then
+		t = false
 	end
 end)
